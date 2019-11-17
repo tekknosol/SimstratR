c5586b67fe7fdcd08475dc100e6b058d0199cfa9
Jorrit Mesman <42146440+jorritmesman@users.noreply.github.com>
2019-11-17 15:38:21 +0100

    Added verbose option to Windows and prepared Mac-code for testing

--- R/run_simstrat.R
+++ R/run_simstrat.R
@@ -15,28 +15,18 @@
 #'run_simstrat(sim_folder, par_file = 'langtjern.par')
 #'@export
 #'@importFrom utils packageName
-run_simstrat <- function (sim_folder = ".", par_file="langtjern.par", verbose = FALSE)
+run_simstrat <- function (sim_folder = ".", par_file="langtjern.par", verbose = TRUE)
 {
-  
+  ### Windows
   if (.Platform$pkgType == "win.binary") {
-    return(run_simstratWin(sim_folder, par_file))
+    return(run_simstratWin(sim_folder, par_file, verbose))
   }
-  ### unix-based '''
+  
+  ### UNIX
   if (.Platform$pkgType == "source") {
-    return(run_simstratNIX(sim_folder, par_file))
+    return(run_simstratNIX(sim_folder, par_file, verbose))
   }
-  ### macOS ###
-  if (grepl('mac.binary',.Platform$pkgType)) {
-    maj_v_number <- as.numeric(strsplit(
-      Sys.info()["release"][[1]],'.', fixed = TRUE)[[1]][1])
-    
-    if (maj_v_number < 13.0) {
-      stop('pre-mavericks mac OSX is not supported. Consider upgrading')
-    }
-    
-    return(run_simstratNIX(sim_folder, par_file))
-    
-  }
+
   # ### macOS ###
   # if (grepl('mac.binary',.Platform$pkgType)) {
   #   maj_v_number <- as.numeric(strsplit(
@@ -46,19 +36,15 @@ run_simstrat <- function (sim_folder = ".", par_file="langtjern.par", verbose = 
   #     stop('pre-mavericks mac OSX is not supported. Consider upgrading')
   #   }
   # 
-  #   return(run_gotmOSx(sim_folder, verbose, system.args))
+  #   return(run_simstratOSx(sim_folder, par_file, verbose))
   # 
-  # }
-  # 
-  # if (.Platform$pkgType == "source") {
-  #   return(run_gotmNIX(sim_folder, verbose, args))
   # }
 }
 
-run_simstratWin <- function(sim_folder,par_file="simstrat.par"){
+run_simstratWin <- function(sim_folder,par_file="simstrat.par",verbose=TRUE){
   
   if(.Platform$r_arch == 'x64'){
-    simstrat_path <- system.file('extbin/simstrat.exe', package = 'SimstratR') #packageName()
+    simstrat_path <- system.file('extbin/simstrat.exe', package = 'SimstratR')
   }else{
     stop('No Simstrat executable available for your machine yet...')
   }
@@ -80,43 +66,38 @@ run_simstratWin <- function(sim_folder,par_file="simstrat.par"){
     setwd(origin)
   })
 }
-# 
-# # run_gotmOSx <- function(sim_folder, yaml = TRUE, yaml_file = 'gotm.yaml', verbose = TRUE, args){
-# #   #lib_path <- system.file('extbin/macGOTM/bin', package=packageName()) #Not sure if libraries needed for GOTM
-# #
-# #   gotm_path <- system.file('exec/macgotm', package=packageName())
-# #
-# #   # ship gotm and libs to sim_folder
-# #   #Sys.setenv(DYLD_FALLBACK_LIBRARY_PATH=lib_path) #Libraries?
-# #
-# #   if(yaml){
-# #     args <- c(args, yaml_file)
-# #   }else{
-# #     args <- c(args,'--read_nml')
-# #   }
-# #
-# #   origin <- getwd()
-# #   setwd(sim_folder)
-# #
-# #   tryCatch({
-# #     if (verbose){
-# #       out <- system2(gotm_path, wait = TRUE, stdout = "",
-# #                      stderr = "", args = args)
-# #
-# #     } else {
-# #       out <- system2(gotm_path, wait = TRUE, stdout = NULL,
-# #                      stderr = NULL, args=args)
-# #     }
-# #
-# #     setwd(origin)
-# # 	return(out)
-# #   }, error = function(err) {
-# #     print(paste("GOTM_ERROR:  ",err))
-# #
-# #     setwd(origin)
-# #   })
-# # }
-# 
+
+# run_simstratOSx <- function(sim_folder, par_file = 'langtjern.par', verbose=TRUE){
+#
+#   # simstrat_path <- system.file('extbinmac/simstrat.exe', package = 'SimstratR')
+#   # # File to the folder with the mac-executable for simstrat
+#
+#   # ship gotm and libs to sim_folder
+#   #Sys.setenv(DYLD_FALLBACK_LIBRARY_PATH=lib_path) #Libraries?
+#
+#
+#   origin <- getwd()
+#   setwd(sim_folder)
+#   
+#   tryCatch({
+#     if (verbose){
+#       out <- system2(simstrat_path, wait = TRUE, stdout = "",
+#                      stderr = "", args = par_file)
+#
+#     } else {
+#       out <- system2(simstrat_path, wait = TRUE, stdout = NULL,
+#                      stderr = NULL, args=par_file)
+#     }
+#
+#     setwd(origin)
+# 	return(out)
+#   }, error = function(err) {
+#     print(paste("GOTM_ERROR:  ",err))
+#
+#     setwd(origin)
+#   })
+# }
+
 run_simstratNIX <- function(sim_folder, par_file = 'langtjern.par', verbose=TRUE){
   simstrat_path <- system.file('exec/nixsimstrat', package= 'SimstratR')
 
@@ -137,6 +118,25 @@ run_simstratNIX <- function(sim_folder, par_file = 'langtjern.par', verbose=TRUE
     print(paste("Simstrat_ERROR:  ",err))
     setwd(origin)
   })
+  # origin <- getwd()
+  # setwd(sim_folder)
+  # # Sys.setenv(LD_LIBRARY_PATH=system.file('extbin/nixsimstrat',
+  #                                        # package=packageName()))
+  # 
+  # tryCatch({
+  #   if (verbose){
+  #     out <- system2(simstrat_path, wait = TRUE, stdout = "",
+  #                    stderr = "", args=args)
+  #   } else {
+  #     out <- system2(simstrat_path, wait = TRUE, stdout = NULL,
+  #                    stderr = NULL, args = args)
+  #   }
+  #   setwd(origin)
+  #   return(out)
+  # }, error = function(err) {
+  #   print(paste("SIMSTRAT_ERROR:  ",err))
+  #   setwd(origin)
+  # })
 }
 # 
 # ### From GLEON/gotm3r
